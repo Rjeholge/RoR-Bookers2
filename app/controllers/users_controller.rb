769ce68cce991e917abeb02930875f60d968fdd0
Rjeholge <111187701+Rjeholge@users.noreply.github.com>
2022-09-20 09:05:46 +0000

    [Update]view作成

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,17 @@
+class UsersController < ApplicationController
+  def index
+   @users = User.all
+  end
+
+  def show
+  end
+
+  def edit
+   @user = User.find(params[:id])
+  end
+
+  private
+  def user_params
+   params.require(:user).permit(:name, :introduction)
+  end
+end
